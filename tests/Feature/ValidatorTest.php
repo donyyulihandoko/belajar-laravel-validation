fb04317afaad58882a5c2993f016c45c222b583f
Dony Yuli Handoko <officialdonyyulihandoko@gmail.com>
2025-09-15 12:51:30 +0700

    commit pertemuan1

--- tests/Feature/ValidatorTest.php
+++ tests/Feature/ValidatorTest.php
@@ -4,12 +4,14 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ValidatorTest extends TestCase
 {
-    public function testValidation()
+    public function testValidator()
     {
         $data = [
             'username' => 'donyyulihandoko',
@@ -23,5 +25,100 @@ class ValidatorTest extends TestCase
         $validation = Validator::make($data, $rules);
 
         self::assertNotNull($validation);
+        self::assertTrue($validation->passes());
+        self::assertFalse($validation->fails());
+    }
+
+
+    public function testValidatorInvalid()
+    {
+        $data = [
+            'username' => '',
+            'password' => ''
+        ];
+        $rules = [
+            'username' => ['required'],
+            'password' => ['required']
+        ];
+
+        $validation = Validator::make($data, $rules);
+
+        self::assertNotNull($validation);
+        self::assertFalse($validation->passes());
+        self::assertTrue($validation->fails());
+
+        $message = $validation->getMessageBag();
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidatorValidationException()
+    {
+        $data = [
+            'username' => '',
+            'password' => ''
+        ];
+        $rules = [
+            'username' => ['required'],
+            'password' => ['required']
+        ];
+
+        $validation = Validator::make($data, $rules);
+
+        try {
+            $validation->validate();
+            self::fail('Validation error is not thrown');
+        } catch (ValidationException $exception) {
+            self::assertNotNull($exception->validator);
+            $message = $exception->validator->errors();
+            Log::error($message->toJson(JSON_PRETTY_PRINT));
+        }
+    }
+
+    public function testValidatorMultipleRules()
+    {
+        $data = [
+            'username' => 'dony',
+            'password' => 'dony'
+        ];
+        $rules = [
+            'username' => "required|email|max:100",
+            'password' => ['required', 'min:6', 'max:20']
+        ];
+
+        $validation = Validator::make($data, $rules);
+
+        self::assertNotNull($validation);
+        self::assertFalse($validation->passes());
+        self::assertTrue($validation->fails());
+
+        $message = $validation->getMessageBag();
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidatorValidData()
+    {
+        $data = [
+            'username' => 'dony@gmail.com',
+            'password' => 'password',
+            'admin' => true,
+            'others' => 'xxxxx'
+        ];
+
+        $rules = [
+            'username' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'min:6', 'max:20']
+        ];
+
+        $validation = Validator::make($data, $rules);
+
+        try {
+            $data = $validation->validate();
+            Log::info(json_encode($data, JSON_PRETTY_PRINT));
+            self::assertNotNull($data);
+        } catch (ValidationException $exception) {
+            self::assertNotNull($exception->validator);
+            $message = $exception->validator->errors();
+            Log::error($message->toJson(JSON_PRETTY_PRINT));
+        }
     }
 }
